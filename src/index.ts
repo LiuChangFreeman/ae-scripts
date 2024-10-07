2b0756acef27147478a71bcbc6098288fd4331b4
liuchang.freemen <liuchang.freemen@bytedance.com>
2024-10-07 18:17:09 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,34 @@
 import "./init"
 import { toArray } from "./lib/collection"
 
-
-
-// a standard javascript array example:
-function displayCompLayerNames() {
-    const items = toArray<ItemCollection, Item>(app.project.items)
-    const comp = items.find((item) => item instanceof CompItem)
-
-    if (comp instanceof CompItem) {
-        const layers = toArray<LayerCollection, Layer>(comp.layers)
-        // writeLn(JSON.stringify({ time: layers[0].startTime }))
-        // comp.layers.addText("fasfasfasf")
+function replaceWords() {
+    const project = app.project
+    const compName = NX.get("comp") as string || "main"
+    const stretch = NX.get("stretch") as number || 100
+    const items = toArray<ItemCollection, Item>(project.items)
+    const comp = items.find((item) => item instanceof CompItem && item.name == compName)
+    if (comp !== undefined) {
+        const item = comp as CompItem
+        const layers = toArray<LayerCollection, AVLayer>(item.layers)
+        const textLayers = ["text", "text_main", "text_sub"];
         for (const layer of layers) {
-            // const scaleProperty = layer.scale //定义缩放
-            // const time = 3.5
-            // const scaleValue = scaleProperty.valueAtTime(time, false) //获取3.5秒的缩放值
-            // writeLn(`${layer.name} scale at ${time}: ${scaleValue[0]}/${scaleValue[1]}`)
-            app.beginUndoGroup("rotation")
-            const rotationProperty = layer.rotation //定义旋转
-            rotationProperty.setValueAtTime(0, 0)
-            rotationProperty.setValueAtTime(1.5, 90)
-            rotationProperty.setValueAtTime(3.0, 0)
-            app.endUndoGroup()
+            layer.stretch = stretch
+            const layerName = layer.name
+            if (textLayers.indexOf(layerName) !== -1) {
+                const text = NX.get(layerName) as string || ""
+                const layerText = layer as TextLayer
+                const textProp = layerText.property("Source Text") as Property
+                textProp.setValue(new TextDocument(text))
+            }
         }
     }
 }
 
-
 function renderByNx() {
     const project = app.project
-
+    const compName = NX.get("comp") as string || "main"
     const items = toArray<ItemCollection, Item>(project.items)
-    const comp = items.find((item) => item instanceof CompItem && item.name == "main")
+    const comp = items.find((item) => item instanceof CompItem && item.name == compName)
     if (comp !== undefined) {
         const renderQueue = project.renderQueue
         const renderQueueItems = toArray<RQItemCollection, RenderQueueItem>(renderQueue.items)
@@ -51,56 +47,11 @@ function renderByNx() {
                 "Full Flat Path": outputPath
             }
         };
-
         outputModule.setSettings(settings);
         renderQueue.render()
     }
 
 }
 
-
-function changeLayerTime() {
-    const project = app.project
-
-    const items = toArray<ItemCollection, Item>(project.items)
-    const comp = items.find((item) => item instanceof CompItem && item.name == "video")
-    if (comp !== undefined) {
-        const item = comp as CompItem
-        const layer = item.layer(1) as AVLayer
-        layer.startTime = 2.0
-        layer.inPoint = 5.0
-        layer.outPoint = 10.0
-        const time = `time: ${layer.inPoint}-${layer.outPoint}-${layer.startTime}`
-        writeLn(time)
-    }
-}
-
-function replaceWords() {
-    const project = app.project
-
-    const items = toArray<ItemCollection, Item>(project.items)
-    const comp = items.find((item) => item instanceof CompItem && item.name == "main")
-    if (comp !== undefined) {
-        const item = comp as CompItem
-        const layers = toArray<LayerCollection, AVLayer>(item.layers)
-        const textLayers = ["text", "text_main", "text_sub"];
-        for (const layer of layers) {
-            const stretch = NX.get("stretch") as number
-            layer.stretch = stretch
-            const name = layer.name
-            if (textLayers.indexOf(name) !== -1) {
-                const text = NX.get(name) as string
-                const layerText = layer as TextLayer
-                const textProp = layerText.property("Source Text") as Property
-                textProp.setValue(new TextDocument(text))
-            }
-        }
-    }
-}
-
-
-// displayCompLayerNames()
-// renderByNx()
-// changeLayerTime()
 replaceWords()
 renderByNx()
